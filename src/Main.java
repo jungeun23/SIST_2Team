67fc445df07386efd91fc396cb1e32aaa04c6919
leejunheee <z6z6jun@naver.com>
2021-05-01 16:08:37 +0900

    temp

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,14 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		Contact user = new Contact();
+		//Contact user = new Contact();
 //		user.regist_user();
-		user.updateDB();
+		//user.updateDB();
 //		System.out.println(user);
+		
+		Mail mail = new Mail();
+		mail.getReceiverEmail(mail.name);
+		System.out.println(mail.receiveEmail);
 		
 	}
 }
