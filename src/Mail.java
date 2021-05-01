2902ebf1b7d2d7190147760e45f83b7db02edd06
leejunheee <z6z6jun@naver.com>
2021-05-01 18:10:57 +0900

    Mail 클래스

--- src/Mail.java
+++ src/Mail.java
@@ -1,3 +1,4 @@
+import java.awt.print.Printable;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -7,10 +8,8 @@ import java.util.Scanner;
 
 /**
  * 
- * @author 2조 
- * 			  통합 Mail 기능 클래스
- * 			  1. 메일 쓰기 - 수신자 검색 -> 해당하는 mail주소 가져오기 -> 내용입력 후 새로운 txt 파일로 저장. 
- *       	  2. 메일 읽기 - 내 메일을 찾아 내용 읽기 
+ * @author 2조 통합 Mail 기능 클래스 1. 메일 쓰기 - 수신자 검색 -> 해당하는 mail주소 가져오기 -> 내용입력 후 새로운
+ *         txt 파일로 저장. 2. 메일 읽기 - 내 메일을 찾아 내용 읽기
  */
 public class Mail {
 	//
@@ -20,7 +19,7 @@ public class Mail {
 	private String receiverEmail;
 	private String senderName;
 	private String receiverName;
-	private Contact user;
+	private User user;
 
 	public Mail() {
 
@@ -30,28 +29,22 @@ public class Mail {
 		receiverEmail = "";
 		receiverName = "";
 		senderName = "";
-		user = new Contact();
 	}
 
+	public Mail(User user) {
 
-	public Mail(Contact user) {
-		
-		this.senderName = user.getName();
 		DATA = "data\\Contact.txt"; // Window
 		// DATA = "data/Contact.txt"; // Mac
 		scan = new Scanner(System.in);
 		receiverEmail = "";
 		receiverName = "";
-		user = new Contact();
+		user = new User();
 	}
-	
-	
-	
+
 	/**
 	 * @author 2
-	 * @param receiverName : 수신자(메일 받을 사람) 이름 
-	 * @throws IOException
-	 * 수신자의 이름을 찾아서 mail 주소를 저장하는 메소드 
+	 * @param receiverName : 수신자(메일 받을 사람) 이름
+	 * @throws IOException 수신자의 이름을 찾아서 mail 주소를 저장하는 메소드
 	 */
 	public void getReceiverEmail(String receiverName) throws IOException {
 		BufferedReader read = new BufferedReader(new FileReader(DATA));
@@ -64,8 +57,7 @@ public class Mail {
 				break;
 			}
 		} // while
-		
-		
+
 	}
 
 	/**
@@ -75,15 +67,43 @@ public class Mail {
 	public void writeMail() throws IOException {
 		String title = "";
 		String content = "";
+		System.out.print("제목을 입력하세요.");
+		title = scan.nextLine();
+		System.out.print("내용을 입력하세요.");
+		content = scan.nextLine();
+		
+		String path = "data\\mail\\" + this.user.getName() + "_" + this.receiverName;
+//	      path = "data\\mail\\" ;
 
-//		path = "data/mail/" + this.senderName + "_" + this.receiverName + "_" + title;
-//		path = "data\\mail\\";
-
-		FileWriter fw = new FileWriter(DATA);
+		FileWriter fw = new FileWriter(path, true);
 		fw.write(title);
 		fw.write("-----");
 		fw.write(content);
+		fw.write("*****");
 		fw.close();
 
+	}
+
+	public void readMail() throws IOException {
+		String path = "data\\mail\\";
+		File f = new File(path);
+		File[] fl = f.listFiles();
+		String result = "";
+
+		for (int i = 0; i < fl.length; i++) {
+			if (fl[i].isFile()) {
+				String s = fl[i].getName();
+				String t = "";
+				t = s.substring(s.indexOf("_"));
+				if (t.equals(this.user.getName())) {
+					BufferedReader read = new BufferedReader(new FileReader(path + t));
+					String line = "";
+					while ((line = read.readLine()) != null) {
+						result += line + "\r\n";
+					}
+				}
+				System.out.println(result);
+			}
+		}
 	}
 }// Mail
