f6a874aa796b2dca8348c6ec1a0ccfad5727d3b9
HL <kmr345@naver.com>
2021-05-02 15:38:07 +0900

    메일, 전자결재 main 연동 완료

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 	Scanner sc = new Scanner(System.in);
+	private static User user = new User();
 
 	public static void main(String[] args) throws Exception {
-		User user = new User();
 
 //		user.regist_user();
 //		user.updateDB();
@@ -57,7 +58,7 @@ public class Main {
 			int n = Integer.parseInt(Util.get("번호를 입력해주세요"));
 			System.out.println();
 			System.out.println();
-			
+
 			if (n == 0) {
 				break;
 			} else if (n == 1) {
@@ -76,6 +77,7 @@ public class Main {
 
 		}
 	}
+/////////////////////////////////////////////////      Lane2      ///////////////////////////////////////////////
 
 	private static void showContact() {
 		while (true) {
@@ -90,9 +92,9 @@ public class Main {
 			int n = Integer.parseInt(Util.get("번호를 입력해주세요"));
 			System.out.println();
 			System.out.println();
-			
+
 			if (n == 1) {
-//				showMail();
+				showMail();
 			} else if (n == 2) {
 //				showMessenger();
 			} else if (n == 3) {
@@ -121,7 +123,7 @@ public class Main {
 			System.out.println();
 			System.out.println();
 			if (n == 1) {
-//				showElecApproval();
+				showElecApproval();
 			} else if (n == 2) {
 //				showAttendance();
 			} else if (n == 3) {
@@ -129,7 +131,7 @@ public class Main {
 			} else if (n == 4) {
 				menu();
 				break;
-			}else {
+			} else {
 				System.out.println("잘못된 번호를 입력하셨습니다.");
 			}
 		}
@@ -151,7 +153,7 @@ public class Main {
 			} else if (n == 2) {
 				menu();
 				break;
-			}else {
+			} else {
 				System.out.println("잘못된 번호를 입력하셨습니다.");
 			}
 		}
@@ -179,7 +181,7 @@ public class Main {
 			} else if (n == 4) {
 				menu();
 				break;
-			}else {
+			} else {
 				System.out.println("잘못된 번호를 입력하셨습니다.");
 			}
 		}
@@ -207,7 +209,7 @@ public class Main {
 			} else if (n == 4) {
 				menu();
 				break;
-			}else {
+			} else {
 				System.out.println("잘못된 번호를 입력하셨습니다.");
 			}
 		}
@@ -221,26 +223,121 @@ public class Main {
 			System.out.println("            2.  기타");
 			System.out.println();
 			System.out.println("            3.  목차로 돌아가기");
-			
+
 			System.out.println();
 			int n = Integer.parseInt(Util.get("번호를 입력해주세요"));
 			System.out.println();
 			System.out.println();
 			if (n == 1) {
 //				showSecurity();
-			} else if(n==2) {
+			} else if (n == 2) {
 //				showEtcetera();
-			}else if (n == 3) {
+			} else if (n == 3) {
 				menu();
 				break;
-			}else {
+			} else {
 				System.out.println("잘못된 번호를 입력하셨습니다.");
 			}
 		}
 	}
-	
+
+///////////////////////////////////////////////      Lane3      ///////////////////////////////////////////////
+///////////////////////////////////////////////      CONTACT    ///////////////////////////////////////////////
+
+	private static void showMail() {
+		while (true) {
+			cls();
+			System.out.println("            [Mail 업무] ");
+			System.out.println("            1.  메일 쓰기");
+			System.out.println("            2.  메일 읽기");
+			System.out.println("            3.  검색어로 메일 찾기");
+			System.out.println("            4.  Spam 확인");
+			System.out.println();
+			System.out.println("            0.  목차로 돌아가기");
+			System.out.println();
+			int n = Integer.parseInt(Util.get("번호를 입력해주세요"));
+			System.out.println();
+			System.out.println();
+
+			Email mail = new Email(user);
+			if (n == 1) {
+
+				try {
+					mail.writeMail();
+				} catch (IOException e) {
+					System.out.println(e);
+				}
+			} else if (n == 2) {
+				try {
+					mail.readMail();
+				} catch (IOException e) {
+					System.out.println(e);
+				}
+			} else if (n == 3) {
+				try {
+					mail.searchMail();
+				} catch (IOException e) {
+					System.out.println(e);
+				}
+			} else if (n == 0) {
+				showContact();
+				break;
+			} else {
+				System.out.println("잘못된 번호를 입력하셨습니다.");
+			}
+		}
+	}
+
+///////////////////////////////////////////////      APPROVAL    ///////////////////////////////////////////////
+	private static void showElecApproval() {
+		while (true) {
+			cls();
+			System.out.println("            [전자결재 업무] ");
+			System.out.println("            1.  문서 작성");
+			System.out.println("            2.  문서 읽기");
+			System.out.println("            3.  댓글 달기");
+			System.out.println("            4.  내가 작성한 문서 확인");
+			System.out.println("            5.  문서 삭제");
+			System.out.println("            6.  문서 관리(직급별)");
+			System.out.println();
+			System.out.println("            0.  목차로 돌아가기");
+			System.out.println();
+			int n = Integer.parseInt(Util.get("번호를 입력해주세요"));
+			System.out.println();
+			System.out.println();
+
+			ElecApproval ea = new ElecApproval(user);
+			if (n == 1) {
+				try {
+					ea.createElecApproval();
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			} else if (n == 2) {
+				ea.readElecApproval();
+			} else if (n == 3) {
+				ea.addCommnetElecApproval();
+			} else if (n == 4) {
+				ea.myElecApproval();
+			} else if (n == 5) {
+				ea.deleteElecApproval();
+			} else if (n == 6) {
+				try {
+					ea.setElecApprovalCondition();
+				} catch (IOException e) {
+					System.out.println(e);
+				}
+			} else if (n == 0) {
+				showContact();
+				break;
+			} else {
+				System.out.println("잘못된 번호를 입력하셨습니다.");
+			}
+		}
+	}
+
 	private static void cls() {
-		for(int i=0; i<100; i++) {
+		for (int i = 0; i < 100; i++) {
 			System.out.println();
 		}
 	}
