64131cdc8196d0126169604d46f3aa5ae5792cc9
DESKTOP-COJ9858\user <kmr345@naver.com>
2021-05-06 12:30:22 +0900

    a

--- src/Board.java
+++ src/Board.java
@@ -1,17 +1,15 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Board {
 	private static LinkedList<String[]> list = new LinkedList<>();
-	private final String DATA = "data\\board\\board.txt";
-	private User user;
 	private static int seq;
+	private User user;
+	private final String DATA = "data\\board\\board.txt";
 
 	public Board(User user) {
 		this.user = user;
@@ -24,101 +22,156 @@ public class Board {
 			String line = "";
 			while ((line = read.readLine()) != null) {
 				String[] temp = line.split(",");
-				list.add(temp);
+				String content = "";
+				while (!(line = read.readLine()).equals("-----")) {
+					content += line + "\n";
+				}
+
+				String[] t = { temp[0], temp[1], temp[2], temp[3], content };
+				list.add(t);
 			}
+			seq = Util.toInt(list.get(list.size()-1)[0]);
 		} catch (IOException e) {
 			System.out.println(e);
 		}
-
 	}
 
-	public void createBoard() {
+	public void createMessenger() {
 		// 번호 제목 내용 글쓴이 비밀번호
 		String title = Util.get("제목을 입력하세요");
 		String pw = Util.get("비밀번호를 입력하세요");
-		String content = Util.get("내용을 입력하세요");
 		seq++;
-		String[] temp = { Integer.toString(seq), title, pw, this.user.getName(), content, };
+		String content = "";
+		System.out.println("내용을 입력해주세요 (종료 : exit)");
+		while (true) {
+			Scanner sc = new Scanner(System.in);
+			String line = sc.nextLine();
+			if (line.equals("exit"))
+				break;
+			content += line + "\n";
+		}
+
+		String[] temp = { Integer.toString(seq), title, this.user.getName(), content };
 		list.add(temp);
 
 		try {
 			FileWriter fw = new FileWriter(DATA, true);
 			fw.write(seq + ",");
 			fw.write(title + ",");
-			fw.write(pw + ",");
-			fw.write(this.user.getName() + ",");
-			fw.write(content + "\n");
+			fw.write(this.user.getName() + "\n");
+			fw.write(content);
+			fw.write("-----");
+			fw.write("\n");
 			fw.close();
 		} catch (IOException e) {
 			System.out.println(e);
 		}
 
 		System.out.println("글 작성을 완료했습니다.");
-
 	}
 
-	public void readBoard() {
+	public void readMessenger() {
+		// 번호 제목 보내는사람 내용
 		for (int i = 0; i < list.size(); i++) {
-			System.out.printf("[%s] %s %s\n\n%s", list.get(i)[0], list.get(i)[1], list.get(i)[3], list.get(i)[4]);
+			System.out.printf("[%s] %s\n", list.get(i)[0], list.get(i)[1]);
+		}
+
+		int c = Util.toInt(Util.get("읽을 메세지를 선택하세요"));
+		for (int i = 0; i < list.size(); i++) {
+			if (Util.toInt(list.get(i)[0]) == c) {
+				System.out.println();
+				System.out.println("메세지를 출력합니다.");
+				System.out.printf("[%s] %s\n%s :\n%s", list.get(i)[0], list.get(i)[1], list.get(i)[2], list.get(i)[3]);
+			}
 		}
 	}
 
-	public void deleteBoard() {
-		readBoard();
-		int choice = Util.toInt(Util.get("삭제할 번호를 입력하세요 : "));
-		choice--;
+	public void updateMessenger() {
+		// 번호 제목 보내는사람 내용
 		for (int i = 0; i < list.size(); i++) {
-			if (i == choice) {
-				System.out.println(list.get(i)[1] + " 게시물을 삭제했습니다.");
+			System.out.printf("[%s] %s\n", list.get(i)[0], list.get(i)[1]);
+		}
+
+		int c = Util.toInt(Util.get("수정할 메세지를 선택하세요"));
+		for (int i = 0; i < list.size(); i++) {
+			if (Util.toInt(list.get(i)[0]) == c) {
+				String title = Util.get("제목을 입력하세요");
+				seq++;
+				String content = "";
+				System.out.println("내용을 입력해주세요 (종료 : exit)");
+				while (true) {
+					Scanner sc = new Scanner(System.in);
+					String line = sc.nextLine();
+					if (line.equals("exit"))
+						break;
+					content += line + "\n";
+				}
+				String[] temp = { Integer.toString(seq), title, this.user.getName(), content };
 				list.remove(i);
+				list.add(temp);
 			}
-		}
+		} // exit for
+		FileWriter fw = null;
 		try {
-			FileWriter fw = new FileWriter(DATA);
-			for (int i = 0; i < list.size(); i++) {
-				fw.write(seq + ",");
+			fw = new FileWriter(DATA);
+		} catch (IOException e1) {
+			System.out.println(e1);
+		}
+		for (int i = 0; i < list.size(); i++) {
+			try { 
+				fw.write(list.get(i)[0] + ",");
 				fw.write(list.get(i)[1] + ",");
-				fw.write(list.get(i)[3] + ",");
-				fw.write(this.user.getName() + ",");
-				fw.write(list.get(i)[4] + "\n");
-				fw.close();
+				fw.write(list.get(i)[2] + "\n");
+				fw.write(list.get(i)[3]);
+				fw.write("-----");
+				fw.write("\n");
+			} catch (IOException e) {
+				System.out.println(e);
 			}
+		} // exit for
+		try {
+			fw.close();
 		} catch (IOException e) {
 			System.out.println(e);
 		}
 	}
 
-	public void updateBoard() {
-		readBoard();
-		int choice = Util.toInt(Util.get("수정할 번호를 입력하세요 : "));
-		choice--;
+	public void deleteMessenger() {
+		// 번호 제목 보내는사람 내용
 		for (int i = 0; i < list.size(); i++) {
-			if (i == choice) {
-				System.out.println(list.get(i)[1] + " 게시물을 삭제했습니다.");
+			System.out.printf("[%s] %s\n", list.get(i)[0], list.get(i)[1]);
+		}
+
+		int c = Util.toInt(Util.get("삭제할 메세지를 선택하세요"));
+		for (int i = 0; i < list.size(); i++) {
+			if (Util.toInt(list.get(i)[0]) == c) {
 				list.remove(i);
 			}
 		}
-		String title = Util.get("제목을 입력하세요");
-		String pw = Util.get("비밀번호를 입력하세요");
-		String content = Util.get("내용을 입력하세요");
-		seq++;
-		String[] temp = { Integer.toString(seq), title, pw, this.user.getName(), content, };
-		list.add(temp);
 
+		FileWriter fw = null;
 		try {
-			FileWriter fw = new FileWriter(DATA);
-			for (int i = 0; i < list.size(); i++) {
-				fw.write(seq + ",");
-				fw.write(list.get(i)[1] + ",");
-				fw.write(list.get(i)[3] + ",");
-				fw.write(this.user.getName() + ",");
-				fw.write(content + "\n");
-				fw.close();
-				System.out.println("게시물 수정 완료 됐습니다.");
-			}
-		} catch (IOException e) {
-			System.out.println(e);
+			fw = new FileWriter(DATA);
+		} catch (IOException e1) {
+			System.out.println(e1);
 		}
-	}
+		for (int i = 0; i < list.size(); i++) {
+			try {
+				fw.write(list.get(i)[0] + ",");
+				fw.write(list.get(i)[1] + ",");
+				fw.write(list.get(i)[2] + "\n");
+				fw.write(list.get(i)[3]);
+				fw.write("-----");
+				fw.write("\n");
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+			try {
+				fw.close();
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+		} // exit for
 
+	}
 }
