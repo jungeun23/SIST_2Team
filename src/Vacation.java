121bfe3e4952ca379b958fd43cbcd04d131b6411
MOON_ <moonj0522@gmail.com>
2021-05-10 14:14:30 +0900

    도큐먼트 추가중

--- src/Vacation.java
+++ src/Vacation.java
@@ -12,8 +12,6 @@ import java.util.Scanner;
 /**
  * 휴가관리 구현 클래스
  * 사용자는 휴가를 신청, 취소하거나 신청한 휴가일정을 확인할 수 있다. 
- * @param user 현재 사용중인 유저의 정보를 식별하기 위한 변수
- * @param mc 캘린더 구현부를 가져오는 변수
  */
 public class Vacation {
 	User user;
@@ -24,36 +22,21 @@ public class Vacation {
 		this.mc = new MyCalendar_jiwon(user);
 	}
 	
-
 	public void firstScreen() {
 		mc.firstScreen();
 	}
 	
-	/**
-	 * 휴가날짜와 휴가사유를 입력받고, 그걸 휴가일정 파일(DATA2)에 저장하는 메소드
-	 */
 	public void createVacation(){
 		mc.createScheduleVacation();
 	}
 	
-	/**
-	 * 입력했던 휴가날짜를 모두 출력하고, 거기에서 취소할 일정을 입력받는다.
-	 * 입력받은 취소 일정을 휴가일정 파일(DATA2)에서 지우는 메소드.
-	 */
 	public void deleteVacation() {
 		mc.deleteVacation();
 	}
 	
-	
-	/**
-	 * 입력했던 휴가날짜를 모두 출력하고, 거기에서 확인할 일정을 입력받는다.
-	 * 확인할 일정은 휴가 사유와 함께 출력하는 메소드. 
-	 */
 	public void readVacation() {
 		mc.readVacation();
 	}
-	
-	
 }//Class
 
 
